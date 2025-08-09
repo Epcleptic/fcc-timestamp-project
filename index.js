ec386e6627d5dde8099bb0807da1ce9a78bc230b
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2025-08-09 19:33:19 +0200

    Use dynamic port (accidentally reverted this)

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.get("/api/:input", function (req, res) {
 });
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
